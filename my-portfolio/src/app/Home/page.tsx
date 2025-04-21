a656e556a223f72d30ef11c6ed136fbbbfe882e5
Zonikhan <150387908+programmingwithAbdullah125@users.noreply.github.com>
2025-04-22 01:27:50 +0500

    my-portfolio-00
    portfolio

---
+++ my-portfolio/src/app/Home/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            {/* Main div */}
+            <div className="flex flex-col md:flex-row justify-between items-center mt-36 px-6"> {/* Mobile-first layout */}
+
+                {/* Left side section */}
+                <div className="space-y-4 max-w-2xl text-center md:text-left md:w-1/2 pl-9">
+                    <h3 className="text-4xl font-light text-gray-400">Hi, I'm</h3>
+                    <h1 className="text-5xl font-bold text-white mt-2">Abdullah Ateeq</h1>
+                    <h2 className="text-2xl font-semibold text-white">Frontend Developer</h2>
+                    <p className="text-lg text-gray-200 mt-4">
+                        I'm a Frontend Developer with experience in HTML, CSS, JAVASCRIPT, TYPESCRIPT, NEXT.JS, TAILWIND CSS, and specialized in building responsive and dynamic websites. Currently learning Artificial Intelligence, Agentic & Robotic AI.
+                    </p>
+                </div>
+
+                {/* Right side section (Image) */}
+                <div className="mt-6 md:mt-0 md:w-1/3">
+                    <Image
+                        src="/profile.jpg"  // Replace with your image in the 'public' folder
+                        alt="Abdullah's profile"
+                        width={300} // Adjust size as needed
+                        height={300} // Adjust size as needed
+                        className="rounded-full"
+                    />
+                </div>
+
+            </div>
+        </>
+    )
+  }
---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Home from "./Home/page";
+import Navbar from "./NavBar/page";
+
+ export default function MainPage() {
+    return(
+      <>
+      <Navbar/>
+      <Home/>
+      </>
+    )
+}
